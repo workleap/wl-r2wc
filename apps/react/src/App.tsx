5b8a45763b81de68104b5cf9a59095c2bfd37482
Mahmoud Moravej <moravej@gmail.com>
2024-10-03 18:56:35 -0400

    implement react-router

--- apps/react/src/App.tsx
+++ apps/react/src/App.tsx
@@ -1,71 +1,13 @@
-import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { MainPage } from "./MainPage.tsx";
+import { StorePage } from "./StorePage.tsx";
 
-interface MovieWidgetsConfig {
-    initialize: () => void;
-}
-
-declare global {
-    interface Window {
-        MovieWidgets?: MovieWidgetsConfig;
-    }
-}
 
 export function App() {
-    const [theme, setTheme] = useState("dark");
-
-    useEffect(() => {
-        window.MovieWidgets?.initialize();
-    }, []);
-
     return (
-        <div style={{ display: "flex" }}>
-            <div style={{
-                flex: 1,
-                margin: "50px",
-                border: "5px solid gray",
-                padding: "5px"
-            }}
-            >
-                <h3 style={{ textAlign: "center" }}> Widget Area</h3>
-                <wl-movie-details onClick={() => console.log("hello")} show-ranking="true" ></wl-movie-details>
-                <wl-movie-context theme={theme} ></wl-movie-context>
-            </div>
-            <div style={{ flex: 1, margin: "50px", border: "5px solid", padding: "5px" }}>
-                <h3 style={{ textAlign: "center" }}>Host app area</h3>
-                <button type="button" onClick={() => setTheme(theme === "light" ? "dark" : "light")}>ChangeTheme!</button>
-            </div>
-            <div style={{ flex: 1, margin: "50px", border: "5px solid", padding: "5px" }}>
-                <h3 style={{ textAlign: "center" }}> Widget Area</h3>
-                <wl-movie-pop-up text="Click Me!!"></wl-movie-pop-up>
-            </div>
-        </div>
+        <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/store" element={<StorePage />} />
+        </Routes>
     );
-}
-
-declare global {
-    // eslint-disable-next-line @typescript-eslint/no-namespace
-    namespace JSX {
-        interface IntrinsicElements {
-            "wl-movie-pop-up": React.DetailedHTMLProps<
-            React.HTMLAttributes<HTMLElement> & {
-                text?: string;
-            },
-            HTMLElement
-            >;
-
-            "wl-movie-details": React.DetailedHTMLProps<
-            React.HTMLAttributes<HTMLElement> & {
-                "show-ranking"?: string;
-            },
-            HTMLElement
-            >;
-
-            "wl-movie-context": React.DetailedHTMLProps<
-            React.HTMLAttributes<HTMLElement> & {
-                theme?: string;
-            },
-            HTMLElement
-            >;
-        }
-    }
 }
