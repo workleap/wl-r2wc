d0c007c7bad79ecf355158a5ace1727707024808
Mahmoud Moravej <moravej@gmail.com>
2024-10-16 13:02:38 -0400

    change some type definitions

--- widgets/src/r2wc/utils.ts
+++ widgets/src/r2wc/utils.ts
@@ -1,33 +1,31 @@
 
 
-interface MapItemObject<Props> {
+type MapItem<Props> = {
     to: keyof Props;
     convert?: (value: string | null) => Props[keyof Props];
-}
+} | keyof Props;
 
-type MapItem<Props> = MapItemObject<Props> | keyof Props;
-
-type FunctionKeys<T> = {
-    [K in keyof T]: T[K] extends (((...args: never[]) => unknown) | undefined) ? K : never;
+type ConditionnalKeys<T, ExpectedValue> = {
+    [K in keyof T]: T[K] extends ExpectedValue ? K : never;
 }[keyof T];
 
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+type FunctionKeys<T> = ConditionnalKeys<T, Function>;
+
 export interface Map<Props, ObservedAttributesType extends string> {
-    attributes?: {
-        [K in ObservedAttributesType]: MapItem<Props>;
-    };
-    events?:{
-        [key: string]: FunctionKeys<Props>;
-    };
+    attributes?: Record<ObservedAttributesType, MapItem<Props>>;
+    events?: Record<string, MapItem<Props>>;
 }
 
-export function getMapName<Props>(map: MapItem<Props> | FunctionKeys<Props>): keyof Props {
+export function getMapName<Props>(map: MapItem<Props> | FunctionKeys<Props>) {
     if (typeof map === "object") {
         return map.to;
     }
 
     return map;
 }
-export function getMapConvert<Props>(map: MapItem<Props>): MapItemObject<Props>["convert"] {
+export function getMapConvert<Props>(map: MapItem<Props>) {
     if (typeof map === "object") {
         return map.convert;
     }
