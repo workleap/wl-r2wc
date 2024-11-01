939f0965ff934034ee90f78b918155648f9e6479
Mahmoud Moravej <moravej@gmail.com>
2024-11-01 16:40:17 -0400

    refactor folder and package structures

---
+++ packages/r2wc/src/index.ts
@@ -0,0 +1,4 @@
+export { type Map } from "./utils.ts";
+export { WebComponentHTMLElement, WebComponentHTMLElementBase } from "./WebComponentHTMLElement.tsx";
+export { WidgetsManager } from "./WidgetsManager.tsx";
+
